f3405684e7d799864efe747185dcb3184e22e4f1
Asif Ashadullah <156141923+asha0012-asif@users.noreply.github.com>
2025-08-20 17:05:01 -0400

    feat(explore): create fetchGameByID function in GameContext connecting to /games/:id API endpoint

--- src/components/layouts/GameCard.tsx
+++ src/components/layouts/GameCard.tsx
@@ -2,6 +2,7 @@ import { TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { styles } from "@/src/theme/styles";
 
+import { useGameContext } from "@/src/context/GameContext"; // temporary import for testing
 interface GameCardProps {
     game: {
         id: number;
@@ -11,8 +12,13 @@ interface GameCardProps {
 }
 
 const GameCard: React.FC<GameCardProps> = ({ game }) => {
-    function handlePress() {
-        console.log(`Game pressed: ${game["name"]} (${game["id"]})`);
+    // fetchGameByID used on this screen for testing, will be moved into useEffect in GameDetails screen later
+    const { fetchGameByID } = useGameContext();
+
+    async function handlePress() {
+        // will add navigation and passing id through afterwards
+        const gameDetails = await fetchGameByID(game["id"]);
+        console.log(gameDetails);
     }
 
     return (
--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -4,7 +4,6 @@ const GameContext = createContext<any>(null);
 
 function GameContextProvider({ children }: any) {
     const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
-    const [isLoading, setIsLoading] = useState(true);
 
     async function fetchGames(queries: string, limit: number) {
         try {
@@ -26,8 +25,29 @@ function GameContextProvider({ children }: any) {
             return data;
         } catch (error) {
             console.error(error);
-        } finally {
-            setIsLoading(false);
+        }
+    }
+
+    async function fetchGameByID(id: string) {
+        try {
+            const url = `${BASE_URL}/api/games/${id}`;
+            const options = {
+                method: "GET",
+                headers: {
+                    accept: "application/json",
+                },
+            };
+
+            const response = await fetch(url, options);
+            const { data, error } = await response.json();
+
+            if (error) {
+                throw new Error(error);
+            }
+
+            return data;
+        } catch (error) {
+            console.error(error);
         }
     }
 
@@ -51,13 +71,13 @@ function GameContextProvider({ children }: any) {
             return data;
         } catch (error) {
             console.error(error);
-        } finally {
-            setIsLoading(false);
         }
     }
 
     return (
-        <GameContext.Provider value={{ isLoading, fetchGames, searchGames }}>
+        <GameContext.Provider
+            value={{ fetchGames, fetchGameByID, searchGames }}
+        >
             {children}
         </GameContext.Provider>
     );
